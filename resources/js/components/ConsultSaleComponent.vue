b4f7d9a4f8124b590da17d256a3a081df66f1187
condef5 <davis.con.fab@gmail.com>
2019-01-18 09:42:19 -0500

    Add data from backend in ConsultSaleComponent

--- resources/js/components/ConsultSaleComponent.vue
+++ resources/js/components/ConsultSaleComponent.vue
@@ -6,8 +6,16 @@
           <v-card-title class="headline font-weight-regular blue-grey white--text">Inicio</v-card-title>
           <v-card-text>
             <v-subheader class="pa-0">INGRESA TÚ RUC</v-subheader>
-            <v-autocomplete v-model="model" :items="states" :label="`RUC`" persistent-hint></v-autocomplete>
-            <v-btn color="success darken-1" dark>SIGUIENTE</v-btn>
+            <v-autocomplete
+              v-model="model"
+              :items="rucs"
+              :label="`RUC`"
+              persistent-hint
+              item-text="ruc"
+              item-value="database"
+              no-data-text="No se encontro este RUC"
+            ></v-autocomplete>
+            <v-btn color="success darken-1" @click="onSubmit" dark>SIGUIENTE</v-btn>
           </v-card-text>
         </v-card>
       </v-flex>
@@ -20,71 +28,18 @@ export default {
   data() {
     return {
       model: null,
-      states: [
-        "Alabama",
-        "Alaska",
-        "American Samoa",
-        "Arizona",
-        "Arkansas",
-        "California",
-        "Colorado",
-        "Connecticut",
-        "Delaware",
-        "District of Columbia",
-        "Federated States of Micronesia",
-        "Florida",
-        "Georgia",
-        "Guam",
-        "Hawaii",
-        "Idaho",
-        "Illinois",
-        "Indiana",
-        "Iowa",
-        "Kansas",
-        "Kentucky",
-        "Louisiana",
-        "Maine",
-        "Marshall Islands",
-        "Maryland",
-        "Massachusetts",
-        "Michigan",
-        "Minnesota",
-        "Mississippi",
-        "Missouri",
-        "Montana",
-        "Nebraska",
-        "Nevada",
-        "New Hampshire",
-        "New Jersey",
-        "New Mexico",
-        "New York",
-        "North Carolina",
-        "North Dakota",
-        "Northern Mariana Islands",
-        "Ohio",
-        "Oklahoma",
-        "Oregon",
-        "Palau",
-        "Pennsylvania",
-        "Puerto Rico",
-        "Rhode Island",
-        "South Carolina",
-        "South Dakota",
-        "Tennessee",
-        "Texas",
-        "Utah",
-        "Vermont",
-        "Virgin Island",
-        "Virginia",
-        "Washington",
-        "West Virginia",
-        "Wisconsin",
-        "Wyoming"
-      ]
+      rucs: []
     };
   },
   mounted() {
-    axios.get("/api/connections").then(({ data }) => console.log(data));
+    axios.get("/api/connections").then(({ data }) => {
+      this.rucs = data;
+    });
+  },
+  methods: {
+    onSubmit: function() {
+      console.log(this.model);
+    }
   }
 };
 </script>
