bae4b7d12aded8f66b175fbbb3934f2e02136bc3
condef5 <davis.con.fab@gmail.com>
2019-02-01 00:21:33 -0500

    Add relations models with proforma

--- app/Proforma.php
+++ app/Proforma.php
@@ -9,4 +9,14 @@ class Proforma extends Model
     protected $table = 'proformas';
     public $timestamps = false;
     protected $guarded = [];
+
+    public function details()
+    {
+        return $this->hasMany('App\DetailProduct',  'idproforma', 'idproforma');
+    }
+   
+    public function client()
+    {
+        return $this->hasOne('App\Client',  'idcliente', 'idcliente');
+    }
 }
