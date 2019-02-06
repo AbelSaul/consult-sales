476c7087411ef91a5d2b5c79004032f300f01c16
condef5 <davis.con.fab@gmail.com>
2019-02-06 14:52:12 -0500

    Remove extra prices

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,13 +10,13 @@ class ProductController extends Controller
     public function index() {
 
         $products = Product::all();
-        $name_prices = ['precio','precio1','precio2','precio3','precio4','precio_fra','precio_prm'];
+        $name_prices = ['precio','precio1','precio2','precio3','precio4'];
         foreach ($products as $product) {
             $prices = array();
             foreach ($name_prices as $name_price) {
                 $price = $product[$name_price];
                 if($price != null ){
-                    array_push($prices,$price);
+                    array_push($prices, $price);
                 }
             }
             $product->prices = $prices;
