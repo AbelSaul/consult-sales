523ece4aec15f29797db0e19cd9b32965b9c1b60
abelsaul <abelmiraval@gmail.com>
2021-07-01 08:20:26 -0500

    Add new filelds

--- app/Http/Controllers/ProformaController.php
+++ app/Http/Controllers/ProformaController.php
@@ -15,45 +15,48 @@ use View;
 
 class ProformaController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $user = session('user');
-       // $proformas = Proforma::where('fecha', '>=' ,Carbon::now()->format('Y-m-d'))->where('idlocal','=',$user["idlocal"])
-		$proformas = Proforma::where('fecha', '>=' ,Carbon::now())->where('idlocal','=',$user["idlocal"])
-        ->where('idvendedor','=',$user["idpersonal"])
+        // $proformas = Proforma::where('fecha', '>=' ,Carbon::now()->format('Y-m-d'))->where('idlocal','=',$user["idlocal"])
+        $proformas = Proforma::where('fecha', '>=', Carbon::now())->where('idlocal', '=', $user["idlocal"])
+            ->where('idvendedor', '=', $user["idpersonal"])
             ->orderByRaw('idproforma * 1 DESC')->paginate(10);
 
         return view('proformas.index', compact('proformas'));
     }
 
-    public function search(Request $request) {
+    public function search(Request $request)
+    {
         $user = session('user');
 
-       // $startDate = $request->startDate ? $request->startDate : Carbon::now()->format('Y-m-d');
-		$startDate = $request->startDate ? $request->startDate : Carbon::now()->format('Y-m-d');
+        // $startDate = $request->startDate ? $request->startDate : Carbon::now()->format('Y-m-d');
+        $startDate = $request->startDate ? $request->startDate : Carbon::now()->format('Y-m-d');
         $endDate = $request->endDate ?
-            $request->endDate :
-            ($request->startDate ? $request->startDate : Carbon::now()->format('Y-m-d') );
+            $request->endDate : ($request->startDate ? $request->startDate : Carbon::now()->format('Y-m-d'));
 
-        $proformas = Proforma::whereBetween('fecha', [$startDate, $endDate])->where('idlocal','=',$user["idlocal"])
-        ->where('idvendedor','=',$user["idpersonal"])
+        $proformas = Proforma::whereBetween('fecha', [$startDate, $endDate])->where('idlocal', '=', $user["idlocal"])
+            ->where('idvendedor', '=', $user["idpersonal"])
             ->orderByRaw('idproforma * 1 DESC')->with(['client', 'seller'])->paginate(10);
 
         return response()->json($proformas);
     }
 
-    public function edit($id) {
+    public function edit($id)
+    {
 
 
         $proforma = Proforma::where('idproforma', $id)->with(['client', 'details'])->first();
         $details = $proforma->details->toArray();
 
-        $ids = array_map(function($element) {
-                 return $element['idproducto'];
+        $ids = array_map(
+            function ($element) {
+                return $element['idproducto'];
             },
             $details
         );
         $user = session('user');
-       $selected_products = DB::select("SELECT
+        $selected_products = DB::select("SELECT
         productos.idproducto, productos.codigo, productos.codigo2, productos.moneda ,productos.tipo ,productos.tipo_imp,productos.descripcion , productos.precio, productos.precio1, productos.precio2, productos.precio3, productos.precio4,productos.precio_fra ,productos.medida, productos.medida_fra,productos.fraccion, productos.igv, productos.marca  , productos.codigo, productos.descripcion,
         kardex.idlocal, SUM(entrada)-SUM(salida) as stock,
             (SELECT SUM(cantidad)-SUM(canjeado) as reserva
@@ -67,38 +70,38 @@ class ProformaController extends Controller
                                         FROM productos
                                         LEFT JOIN kardex ON productos.idproducto=kardex.idproducto
                                         LEFT JOIN det_pro ON productos.idproducto=det_pro.idproducto
-                                        WHERE (productos.idlocal=1 OR productos.idlocal=0) AND (kardex.idlocal='".$user["idlocal"]."' or kardex.idlocal IS NULL) AND productos.estado='A'  AND productos.idproducto IN (".implode(',',$ids).")
+                                        WHERE (productos.idlocal=1 OR productos.idlocal=0) AND (kardex.idlocal='" . $user["idlocal"] . "' or kardex.idlocal IS NULL) AND productos.estado='A'  AND productos.idproducto IN (" . implode(',', $ids) . ")
                                         AND (YEAR(kardex.fecha)=2019 OR YEAR(kardex.fecha) IS NULL)
 
                                         GROUP BY productos.idproducto
                                          ORDER BY productos.descripcion");
 
 
-       foreach ($selected_products as  $product) {
-           foreach ($details as  $detail) {
-               if ($detail['idproducto'] === $product->idproducto){
-               $product->num_um = $detail['num_um'];
-               $product->precio = $detail['precio'];
-               $product->cantidad = $detail['cantidad'];
-               }
+        foreach ($selected_products as  $product) {
+            foreach ($details as  $detail) {
+                if ($detail['idproducto'] === $product->idproducto) {
+                    $product->num_um = $detail['num_um'];
+                    $product->precio = $detail['precio'];
+                    $product->cantidad = $detail['cantidad'];
+                }
             }
         }
 
-       $selected_detail = json_encode($this->objectConvert($selected_products));
+        $selected_detail = json_encode($this->objectConvert($selected_products));
 
-        return view('proformas.edit', compact('proforma','selected_detail'));
-
+        return view('proformas.edit', compact('proforma', 'selected_detail'));
     }
 
-    public function objectConvert($selected){
+    public function objectConvert($selected)
+    {
         $params = DB::table('parametros')->first();
         $cond = $params->igv_inc == 1;
-        $name_prices = ['precio','precio1','precio2','precio3','precio4'];
+        $name_prices = ['precio', 'precio1', 'precio2', 'precio3', 'precio4'];
         foreach ($selected as $product) {
             $prices = array();
             foreach ($name_prices as $name_price) {
                 $price = $product->$name_price;
-                if($price != null ) {
+                if ($price != null) {
                     $label = $name_price != 'precio' ? str_replace('precio', 'P', $name_price) : 'P0';
                     array_push($prices, [
                         'label' => $label,
@@ -122,7 +125,8 @@ class ProformaController extends Controller
     //     return view('proformas.edit', compact('proforma'));
     // }
 
-    public function create(Request $request) {
+    public function create(Request $request)
+    {
 
         $this->validate($request, [
             'clientId' => 'required',
@@ -134,7 +138,7 @@ class ProformaController extends Controller
 
 
         $user = session('user');
-        $maxIdProforma1 = DB::select("(select max(`idproforma` * 1) as pro from proformas where idlocal='".$user["idlocal"]."' )")[0]->pro + 1;
+        $maxIdProforma1 = DB::select("(select max(`idproforma` * 1) as pro from proformas where idlocal='" . $user["idlocal"] . "' )")[0]->pro + 1;
         $maxIdProforma = DB::select("(select max(`idproforma` * 1) as pro from proformas)")[0]->pro + 1;
 
         // cond_impuesto
@@ -145,7 +149,7 @@ class ProformaController extends Controller
             "idproforma" => $maxIdProforma,
             "idlocal" => $user["idlocal"],
             "idcliente" => $request->clientId,
-            "documento" => $this->createDocument($user["idlocal"],$maxIdProforma1),
+            "documento" => $this->createDocument($user["idlocal"], $maxIdProforma1),
             "fecha" => date("Y-m-d"),
             "hora" => date("h:i:s A"),
             "lista_prec" => 0,
@@ -165,14 +169,16 @@ class ProformaController extends Controller
             "usuario" => $user["name"],
             "correo" => $request->email,
             "telefonos" => $request->phone,
+            "tipo" => "CZ",
+            "fentrega" => "0000-00-00"
         ]);
 
 
         // save products_items
         foreach ($productos as $producto) {
             $price = $producto["tipo"] == 'G' ? $producto["precio"] / (1 +  $producto["igv"] / 100) : $producto["precio"];
-			$tipo_imp = $producto["tipo_imp"] == 'G' ? "10" : "20";
-			DetailProduct::create([
+            $tipo_imp = $producto["tipo_imp"] == 'G' ? "10" : "20";
+            DetailProduct::create([
                 "idproforma" => $proforma->idproforma,
                 "idlocal" => $user["idlocal"],
                 "idproducto" => Product::where("codigo", $producto["codigo"])->first()->idproducto,
@@ -190,23 +196,24 @@ class ProformaController extends Controller
                 "precio_neto" => $price,
                 "importe" => $price * $producto["cantidad"],
                 "tipo" => "P",
-				"marca" => $producto["marca"],
-			    "tipo_afecta" => $tipo_imp,
-				//"tipo_afecta" => $producto["tipo_imp"],
+                "marca" => $producto["marca"],
+                "tipo_afecta" => $tipo_imp,
+                //"tipo_afecta" => $producto["tipo_imp"],
                 "no_stock" => 0,
                 "canjeado" => 0.00,
             ]);
         }
 
         if (request('email')) {
-          //  $user = (object) ["email" => request('email') ];
-          //  \Mail::to($user)->send(new SendProforma($user, $proforma));
+            //  $user = (object) ["email" => request('email') ];
+            //  \Mail::to($user)->send(new SendProforma($user, $proforma));
         }
 
         return response()->json(['message' => "Proforma creada"], 200);
     }
 
-    public function update(Request $request) {
+    public function update(Request $request)
+    {
 
         $this->validate($request, [
             'clientId' => 'required',
@@ -229,7 +236,7 @@ class ProformaController extends Controller
             "idcliente" => $request->clientId,
             "fecha" => date("Y-m-d"),
             "hora" => date("h:i:s A"),
-			"bruto" => $request->total,
+            "bruto" => $request->total,
             "subtotal" => $subTotal,
             "igv" => $request->total - $subTotal,
             "total" => $request->total,
@@ -245,8 +252,8 @@ class ProformaController extends Controller
 
         foreach ($productos as $producto) {
             $price = $producto["tipo"] == 'G' ? $producto["precio"] / (1 +  $producto["igv"] / 100) : $producto["precio"];
-			$tipo_imp = $producto["tipo_imp"] == 'G' ? "10" : "20";
-			DetailProduct::create([
+            $tipo_imp = $producto["tipo_imp"] == 'G' ? "10" : "20";
+            DetailProduct::create([
                 "idproforma" => $proforma->idproforma,
                 "idlocal" => $user["idlocal"],
                 "idproducto" => Product::where("codigo", $producto["codigo"])->first()->idproducto,
@@ -264,9 +271,9 @@ class ProformaController extends Controller
                 "precio_neto" => $price,
                 "importe" => $price * $producto["cantidad"],
                 "tipo" => "P",
-				"marca" => $producto["marca"],
+                "marca" => $producto["marca"],
                 "tipo_afecta" => $tipo_imp,
-				//"tipo_afecta" => $producto["tipo_imp"],
+                //"tipo_afecta" => $producto["tipo_imp"],
                 "no_stock" => 0,
                 "canjeado" => 0.00,
             ]);
@@ -280,23 +287,24 @@ class ProformaController extends Controller
         return response()->json(['message' => "Proforma actualizada"], 200);
     }
 
-	//Aqui se crea el documento con 0001 tendriamos que pasar la serie
-    public function createDocument($idLocal,$pro_max_num) {
+    //Aqui se crea el documento con 0001 tendriamos que pasar la serie
+    public function createDocument($idLocal, $pro_max_num)
+    {
         return "000{$idLocal}-" . str_pad($pro_max_num, 9, "0", STR_PAD_LEFT);
     }
 
-    public function getTotalProformas(Request $request){
-         $user = session('user');
+    public function getTotalProformas(Request $request)
+    {
+        $user = session('user');
 
-		$startDate = $request->startDate ? $request->startDate : Carbon::now()->format('Y-m-d');
+        $startDate = $request->startDate ? $request->startDate : Carbon::now()->format('Y-m-d');
         $endDate = $request->endDate ?
-            $request->endDate :
-            ($request->startDate ? $request->startDate : Carbon::now()->format('Y-m-d') );
+            $request->endDate : ($request->startDate ? $request->startDate : Carbon::now()->format('Y-m-d'));
 
         $proformas = Proforma::whereBetween('fecha', [$startDate, $endDate])
-        ->where('idlocal','=',$user["idlocal"])
-        ->where('idvendedor','=',$user["idpersonal"])
-        ->get();
+            ->where('idlocal', '=', $user["idlocal"])
+            ->where('idvendedor', '=', $user["idpersonal"])
+            ->get();
 
         $total = 0;
         foreach ($proformas as $proforma) {
@@ -304,11 +312,5 @@ class ProformaController extends Controller
         }
 
         return $total;
-        
-            
     }
-
-
-
-
 }
