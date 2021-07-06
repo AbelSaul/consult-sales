3c0813c05e0cafe0f09cfb62c17599482b30f092
abelsaul <abelmiraval@gmail.com>
2021-07-06 08:36:16 -0500

    Comment  code

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,9 +17,9 @@ class ProductController extends Controller
         $year = date("Y");
 
         $products = DB::select("SELECT  productos.idproducto, productos.codigo, productos.codigo2, productos.moneda ,productos.tipo ,productos.tipo_imp,productos.descripcion , productos.precio, productos.precio1, productos.precio2, productos.precio3, productos.precio4,productos.precio_fra ,productos.medida, productos.medida_fra,productos.fraccion, productos.igv, productos.marca  , productos.codigo, productos.descripcion,
-        kardex.idlocal, SUM(entrada)-SUM(salida) as stock,
+        kardex.idlocal, SUM(entrada)-SUM(salida) as stock
 
-        					-- (SELECT SUM(cantidad)-SUM(canjeado)
+        					-- , (SELECT SUM(cantidad)-SUM(canjeado)
         					-- FROM det_pro
         					-- WHERE det_pro.idproducto=productos.idproducto
         					-- GROUP BY det_pro.idproducto) as reserva,
