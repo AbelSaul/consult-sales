df7f24b7e6b95b64f2cd598a7935cf6e47514c66
abelsaul <abelmiraval@gmail.com>
2021-07-06 08:38:01 -0500

    Comment code

--- app/Http/Controllers/ProformaController.php
+++ app/Http/Controllers/ProformaController.php
@@ -61,14 +61,14 @@ class ProformaController extends Controller
         $selected_products = DB::select("SELECT
         productos.idproducto, productos.codigo, productos.codigo2, productos.moneda ,productos.tipo ,productos.tipo_imp,productos.descripcion , productos.precio, productos.precio1, productos.precio2, productos.precio3, productos.precio4,productos.precio_fra ,productos.medida, productos.medida_fra,productos.fraccion, productos.igv, productos.marca  , productos.codigo, productos.descripcion,
         kardex.idlocal, SUM(entrada)-SUM(salida) as stock,
-            (SELECT SUM(cantidad)-SUM(canjeado) as reserva
-            FROM det_pro
-            WHERE det_pro.idproducto=productos.idproducto
-            GROUP BY det_pro.idproducto) as reserva,
-        ((SUM(entrada)-SUM(salida)) - ((
-                                        SELECT SUM(cantidad)-SUM(canjeado) as reserva
-                                        FROM det_pro
-                                        WHERE det_pro.idproducto=productos.idproducto GROUP BY det_pro.idproducto))) as disponible
+        --     ,(SELECT SUM(cantidad)-SUM(canjeado) as reserva
+        --     FROM det_pro
+        --     WHERE det_pro.idproducto=productos.idproducto
+        --     GROUP BY det_pro.idproducto) as reserva,
+        -- ((SUM(entrada)-SUM(salida)) - ((
+        --                                 SELECT SUM(cantidad)-SUM(canjeado) as reserva
+        --                                 FROM det_pro
+        --                                 WHERE det_pro.idproducto=productos.idproducto GROUP BY det_pro.idproducto))) as disponible
                                         FROM productos
                                         LEFT JOIN kardex ON productos.idproducto=kardex.idproducto
                                         LEFT JOIN det_pro ON productos.idproducto=det_pro.idproducto
